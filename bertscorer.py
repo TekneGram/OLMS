a5b020e6c9ea26f28298abe77220bce0458649d1
Daniel Parsons <parsonsdwp@gmail.com>
2026-09-05 22:23:17 +0900

    feat: add bert scorer that returns a quadratic pairwise comparison

---
+++ bertscorer.py
@@ -0,0 +1,40 @@
+from bert_score import BERTScorer
+import pandas as pd
+
+class BertScore:
+  def __init__(
+      self,
+      text_a: list[str],
+      text_b: list[str]
+  ) -> None:
+    self.text_a = text_a
+    self.text_b = text_b
+    self.scorer = BERTScorer(lang="en", rescale_with_baseline=True)
+
+  def create_bert_score_table(self) -> pd.DataFrame:
+    pairs = [(x, y) for x in self.text_a for y in self.text_b]
+    candidates = [x for x, y in pairs]
+    references = [y for x, y, in pairs]
+
+    precision, recall, f1 = self.scorer.score(candidates, references)
+
+    precision = precision.reshape(len(self.text_a), len(self.text_b))
+    recall = recall.reshape(len(self.text_a), len(self.text_b))
+    f1 = recall.reshape(len(self.text_a), len(self.text_b))
+
+    table = pd.DataFrame(
+      [
+        [
+          [
+            precision[i, j].item(),
+            recall[i, j].item(),
+            f1[i, j].item(),
+          ]
+          for j in range(len(self.text_b))
+        ]
+        for i in range(len(self.text_a))
+      ],
+      index=self.text_a,
+      columns=self.text_b
+    )
+    return table
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from deprel import DependencyParser
 from clause_splitter import ClauseSplitter
-
+from bertscorer import BertScore
 def main() -> None:
   my_text = "The thesis statement clearly presents your opinion, but it needs to explain why the topic matters. Although your thesis statement is focused, it does not fully preview the reasons you discuss in the essay. Your thesis says that school uniforms are good, but it should state your exact position more clearly. To make the thesis stronger, add a reason that connects your opinion to the main argument. Your thesis is understandable because it gives the reader a clear opinion. The thesis would be clearer if you named the two reasons that your body paragraph will explain. You have a thesis statement, but because it is very general, the reader may not know your main argument. Your thesis statement, which appears at the end of the introduction, gives an opinion but does not include a clear reason. I suggest revising the thesis so that it includes both your position and the reason for that position. The thesis is not specific enough to guide the rest of the essay."
   my_text_2 = "The thesis statement clearly presents your opinion. However, it needs to explain why the topic matters. Your thesis statement is focused. However, it does not fully preview the reasons you discuss in the essay. Your thesis says that school uniforms are good. But it should state your exact position more clearly. To make the thesis stronger add a reason. The reason should connect your opinion to the main argument. Your thesis is understandable. This is because it gives the reader a clear opinion. The thesis could be clearer. Name the two reasons that your body paragraph will explain. You have a thesis statement. But it is very general. So the reader may not know your main argument. Your thesis statement gives an opinion. This appears at the end of the introduction. However, it does not include a clear reason. I suggest revising the thesis. It should include both your position and the reason for that position. The thesis is not specific enough to guide the rest of the essay."
@@ -9,10 +9,17 @@ def main() -> None:
     ".models/udpipe"
   )
 
-  parsed = deprel.parse("trial", my_text)
-  splitter = ClauseSplitter(parsed)
-  clauses = splitter.split_all()
-  print(clauses)
+  parsed_1 = deprel.parse("trial", my_text)
+  parsed_2 = deprel.parse("trial_2", my_text_2)
+  splitter_1 = ClauseSplitter(parsed_1)
+  splitter_2 = ClauseSplitter(parsed_2)
+  clauses_1 = splitter_1.split_all()
+  clauses_2 = splitter_2.split_all()
+
+  bertScorer = BertScore(clauses_1, clauses_2)
+  bert_score_table = bertScorer.create_bert_score_table()
+
+  print(bert_score_table)
 
   return
 
