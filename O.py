241a01a3dcc8db79a75d9ee392f60925b830933b
DanielWilliamParsons <parsonsdwp@gmail.com>
2026-09-06 16:42:34 +0900

    feat: add organization measure framework

---
+++ O.py
@@ -0,0 +1,18 @@
+# This script contains classes that calculate the Organization part of the OLMS vector.
+# Kendall's Tau variant b is used here so that it is tie-aware (because clause matches can be many to many)
+from scipy.stats import kendalltau
+
+class O:
+  def __init__(
+      
+  ) -> None:
+    return
+
+  def normalized_tie_aware_tau() -> None:
+    return
+
+  def position_matching() -> None:
+    return
+
+  def sequential_matching() -> None:
+    return
