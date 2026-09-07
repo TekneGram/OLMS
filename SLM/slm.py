2224e8b43ea92e409a65ed81940253dd57abbb42
DanielWilliamParsons <parsonsdwp@gmail.com>
2026-09-07 09:08:32 +0900

    feat: add basic local llm inference

---
+++ SLM/slm.py
@@ -0,0 +1,44 @@
+from llama_cpp import Llama
+import json
+import re
+
+CONTENT = (
+  "You are an essay feedback tutor."
+)
+
+messages = [
+  {
+    "role": "system", "content": CONTENT
+  }
+]
+
+class SLM:
+  def __init__(
+      self
+  ) -> None:
+    self.llm = self._getAI()
+    return
+
+  def _getAI(self) -> Llama:
+    llm = Llama(
+      model_path="",
+      n_ctx=4096,
+      n_threads=8,
+      n_gpu_layers=999,
+      verbose=False
+    )
+    return llm
+
+  def run_inference(self, user_request) -> str:
+    response = self.llm.create_chat_completion(
+      messages=user_request,
+      max_tokens=120,
+      temperature=0.7,
+      top_p=0.95,
+      stream=False
+    )
+
+    llm_message = response["choices"][0]["message"]
+    tutor_message = (llm_message.get("content") or "").strip()
+
+    return tutor_message
